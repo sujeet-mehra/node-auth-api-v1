53db02904a75373c29961f5d25a7578d18b41735
SuJeet Mehra <sujeet@apiit.edu.in>
2025-10-06 15:51:01 +0530

    mysql2 database connection setup

---
+++ src/database/mysql.db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const dbConnection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+dbConnection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database.");
+});
+
+module.exports = dbConnection;
